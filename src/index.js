35ad1091cc7210961d9395a3d271b378981b5435
Leos <highwind26@gmail.com>
2022-06-27 00:33:47 +0900

    Chapter05 End

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Library from './chapter03/Library';
 import Clock from './chapter04/Clock';
+import CommentList from './chapter05/CommentList';
 
-setInterval(() =>{
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
       <App />
-      <Library />
-      <Clock />
+      <CommentList />
     </React.StrictMode>
   );
-}, 1000)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
