9f3a629b1eca6856872b283094da556b7c62f2f4
Leos <highwind26@gmail.com>
2022-06-27 01:06:28 +0900

    Chapter06 End - State LifeCycle 완료

--- src/chapter04/Clock.jsx
+++ src/chapter04/Clock.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const Clock = (props) =>  {
+
+    const Dateclock = () => {
+        let clock = new Date().toLocaleDateString() + " - " + new Date().toLocaleTimeString();
+        return clock;
+    };
+
     return (
         <div>
             <h1>Hi React~</h1>
-            <h2>현제 시간 : { new Date().toLocaleDateString() } { new Date().toLocaleTimeString()}</h2>
+            <h2>현제 시간 : { Dateclock() }</h2>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -2,21 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Library from './chapter03/Library';
 import Clock from './chapter04/Clock';
 import CommentList from './chapter05/CommentList';
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <React.StrictMode>
-      <App />
-      <CommentList />
-    </React.StrictMode>
-  );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  //setInterval(() => {
+    root.render(
+      <React.StrictMode>
+        <App />
+        <Clock />
+        <CommentList />
+      </React.StrictMode>
+    );
+ // }, 1000)
